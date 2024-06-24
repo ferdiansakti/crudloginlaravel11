44b481b67c0cd234aa67b68349bb6c42bfa36b73
ferdiansakti <ferdian@smkdb1jombang.sch.id>
2024-06-25 06:38:16 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+  
+@section('title', 'Welcome to RVH Parfume')
+  
+@section('contents')
+  <div class="row">
+    I make simple CRUD apps with laravel 11 ;-)
+  </div>
+@endsection
